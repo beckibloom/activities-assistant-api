c9c931c4aeba6e23eb4060403a53eafc6bdacc98
Becki Blumer <rblumer13@gmail.com>
2019-11-01 12:00:22 -0500

    updated DATABASE_URL variable name in config and env

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     CLIENT_ORIGIN: '',
-    DB_URL: process.env.DB_URL || 'postgresql://becki_pg:password@localhost/activitiesassistant',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://becki_pg:password@localhost/activitiesassistant',
     JWT_SECRET: process.env.JWT_SECRET || 'AsMarcelProustWouldSay',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
 }
